c3db3b65cf3c445f42c562e7ce418d5feb97bb08
gauravmunjal84 <gauravmunjal84@gmail.com>
2015-03-22 01:27:50 -0600

    Test_Python

---
+++ hhighest_scores_hash.py
@@ -0,0 +1,8 @@
+scores = {}
+result_f = open("results.txt")
+for line in result_f:
+    (name, score) = line.split()
+    scores[float(score)] = name
+result_f.close()
+for key in sorted(scores.keys(), reverse = True):
+    print(scores[key]+ " : " +str(key))
